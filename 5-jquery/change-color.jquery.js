2f8ba7be7a655117626e0471f17db012b73502b9
fab <fab@mail.Fr>
2021-07-02 16:29:47 +0200

    custom plugin jquery

---
+++ 5-jquery/change-color.jquery.js
@@ -0,0 +1,47 @@
+(function($) {
+    $.fn.changeColor = function(options) {
+        // element(s) qui appelle le plugin : this
+        // console.log(this);
+
+        // paramètres par défaut
+        let settings = {
+            delay: 500
+        };
+
+        // fusionner les paramètres passés avec les paramètres par défaut
+        if (options) {
+            $.extend(settings, options);
+        }
+
+        // this ici est l'élément jquery (la sélection d'un ou plusieurs)
+        this.each(function() {
+            // this vaut l'élément javascript
+            let element = this;
+            setInterval(function() {
+                element.style.color = getRandomColor();
+            }, settings.delay);
+        });
+
+        function getRandomColor() {
+            // #09AFF3
+            let chars = "0123456789ABCDEF";
+            let color = "#";
+
+            for (let i = 0; i <= 5; i++) {
+                // générer un indice aléatoire entre 0 et 15
+                let position = randomIntFromInterval(0, 15);
+                // aller chercher le caractère correspondant
+                let char = chars[position];
+                // l'ajouter dans le code couleur
+                color += char;
+                // console.log(color);
+            }
+
+            return color;
+        }
+
+        function randomIntFromInterval(min, max) { // min and max included
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+    };
+})(jQuery);
